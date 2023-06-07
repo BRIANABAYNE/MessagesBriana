eb7f09663761d39f7340126a1a468671b0ebc51d
Briana Bayne <brianabayne@brianas-imac.lan>
2023-06-07 09:45:39 -0600

    completed model

---
+++ MessagesBriana/MessagesBriana/Controllers/MessageController.swift
@@ -0,0 +1,8 @@
+//
+//  MessageController.swift
+//  MessagesBriana
+//
+//  Created by Briana Bayne on 6/7/23.
+//
+
+import Foundation
---
+++ MessagesBriana/MessagesBriana/Models/Message.swift
@@ -0,0 +1,30 @@
+//
+//  Message.swift
+//  MessagesBriana
+//
+//  Created by Briana Bayne on 6/7/23.
+//
+
+import Foundation
+
+class Message {
+    
+    let text: String
+    var isRead: Bool
+    let timeStamp: Date
+    let uuid: UUID
+    
+    init(text: String, isRead: Bool = false, timeStamp: Date = Date(), uuid: UUID = UUID()) {
+        self.text = text
+        self.isRead = isRead
+        self.timeStamp = timeStamp
+        self.uuid = uuid
+    }
+}
+
+// MARK: - Extensions
+extension Message: Equatable {
+    static func == (lhs: Message, rhs: Message) -> Bool {
+        return lhs.uuid == rhs.uuid
+    }
+}
